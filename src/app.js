3a16aa1ac846fc7ffece58d18969c6bdce0eac29
Yura <ybeliaev@gmail.com>
2019-06-08 20:21:26 +0300

    add id

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import React from "react";
 import List from "./list";
 const App = () => {
   const dataList = [
-    { label: "Make fitness", importent: "true" },
-    { label: "Drink vodka", importent: "false" },
-    { label: "Read book", importent: "true" }
+    { label: "Make fitness", importent: "true", id: 1 },
+    { label: "Drink vodka", importent: "false", id: 2 },
+    { label: "Read book", importent: "true", id: 3 }
   ];
   return (
     <div>
--- src/list.js
+++ src/list.js
@@ -2,9 +2,10 @@ import React from "react";
 import ListItem from "./list-item";
 const List = ({ dataList }) => {
   const elements = dataList.map(item => {
+    const { id, ...itemData } = item;
     return (
       <li>
-        <ListItem {...item} />
+        <ListItem key={id} {...itemData} />
       </li>
     );
   });
