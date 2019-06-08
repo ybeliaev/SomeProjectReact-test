168129e55f428af63944a94faace72a3d57c1a29
Yura <ybeliaev@gmail.com>
2019-06-08 19:50:31 +0300

    finish

--- src/list-item.js
+++ src/list-item.js
@@ -1,4 +1,6 @@
 import React from "react";
 
-const ListItem = 
+const ListItem = ({ label }) => {
+  return <span>{label}</span>;
+};
 export default ListItem;
--- src/list.js
+++ src/list.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ListItem from "./list-item";
-
+const List = ({ dataList }) => {
+  const elements = dataList.map(item => {
+    return (
+      <li>
+        <ListItem {...item} />
+      </li>
+    );
+  });
+  return <ul>{elements}</ul>;
+};
 export default List;
