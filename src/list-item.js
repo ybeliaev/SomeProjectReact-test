bbfe0fabe3d77ecb2b7ee9c8b85f36f582fcbc4e
Yura <ybeliaev@gmail.com>
2019-06-11 11:37:59 +0300

    click function

--- src/list-item.js
+++ src/list-item.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const ListItem = ({ label, important = false }) => {
+  function labelClick() {
+    console.log(`Click on '${label}'`);
+  }
   const styleSpan = {
     fontSize: important ? "30px" : "20px",
     color: important ? "tomato" : "black"
   };
   return (
-    <span style={styleSpan} onClick={() => console.log(`Click on '${label}'`)}>
+    <span style={styleSpan} onClick={labelClick}>
       {label}
     </span>
   );
