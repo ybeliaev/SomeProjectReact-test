bb1da40179b8df80ddfeef1dea3b2991dc419d5e
Yura <ybeliaev@gmail.com>
2019-06-11 12:52:14 +0300

    class modify

--- src/list-item.js
+++ src/list-item.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-const ListItem = ({ label, important = false }) => {
-  function labelClick() {
-    console.log(styleSpan["color"]);
+export default class ListItem extends React.Component {
+  labelClick() {
+    console.log(this.styleSpan["color"]);
   }
-  let styleSpan = {
-    fontSize: important ? "30px" : "20px",
-    color: important ? "tomato" : "black"
-  };
-  return (
-    <span style={styleSpan} onClick={labelClick}>
-      {label}
-    </span>
-  );
-};
-export default ListItem;
+  render() {
+    const { label, important } = this.props;
+    const styleSpan = {
+      fontSize: important ? "30px" : "20px",
+      color: important ? "tomato" : "black"
+    };
+    return (
+      <span style={styleSpan} onClick={this.labelClick}>
+        {label}
+      </span>
+    );
+  }
+}
