1564a0983f8e4280eb3be841e9bc9727b29c2dca
Yura <ybeliaev@gmail.com>
2019-06-11 11:27:17 +0300

    click

--- src/list-item.js
+++ src/list-item.js
@@ -5,6 +5,10 @@ const ListItem = ({ label, important = false }) => {
     fontSize: important ? "30px" : "20px",
     color: important ? "tomato" : "black"
   };
-  return <span style={styleSpan}>{label}</span>;
+  return (
+    <span style={styleSpan} onClick={() => console.log(`Click on ${label}`)}>
+      {label}
+    </span>
+  );
 };
 export default ListItem;
