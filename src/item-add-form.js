febca9d06ee84c7ccac9f1e646a255192693408b
Yura <ybeliaev@gmail.com>
2019-06-19 10:58:12 +0300

    add ItemAddForm

---
+++ src/item-add-form.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ItemAddForm = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Input name of element" />
+      <button>Add Item</button>
+    </form>
+  );
+};
+
+export default ItemAddForm;
