1f7330b06d9b071f29d1a4295065205687eac0c9
Jorgen <ybeliaev@gmail.com>
2021-06-28 18:38:14 +0300

    create base Column, Card, App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import styled from 'styled-components'
+import { FC } from 'react'
+
+import { Column } from './Column'
+import { Card } from './Card'
 import { AppContainer } from './styles'
 
-function App() {
-    return <AppContainer>text</AppContainer>
+const App: FC = ({ children }) => {
+    return (
+        <AppContainer>
+            <Column text="To Do">
+                <Card text="Generate app scaffold" />
+            </Column>
+        </AppContainer>
+    )
 }
 
 export default App
