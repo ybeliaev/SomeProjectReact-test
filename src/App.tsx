19e7fa6b07a76b6228759eab9601746d63c1954a
Jorgen <ybeliaev@gmail.com>
2021-06-28 23:29:07 +0300

    add AddItemButtonProps

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,15 @@ import { AppContainer } from './styles'
 const App: FC = () => {
     return (
         <AppContainer>
-            <Column text="Generate app scaffold" />
+            <Column text="To Do">
+                <Card text="Generate app scaffold" />
+            </Column>
+            <Column text="In Progress">
+                <Card text="Learn Typescript.." />
+            </Column>
+            <Column text="Done">
+                <Card text="Begin to use static typing" />
+            </Column>
         </AppContainer>
     )
 }
--- src/Column.tsx
+++ src/Column.tsx
@@ -3,6 +3,7 @@ import { ColumnContainer, ColumnTitle } from './styles'
 
 type ColumnProps = {
     text: string
+    children?: React.ReactNode
 }
 
 export const Column: FC<ColumnProps> = ({ children, text }) => {
